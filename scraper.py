9a6ba0bbc1b3bb144d8f97dd52a29690c2259486
xubik <xubik@users.noreply.github.com>
2014-10-14 23:26:59 +0100

    Initial commit

--- scraper.py
+++ scraper.py
@@ -1,23 +1,51 @@
-# This is a template for a Python scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+import base64
+import scraperwiki
+import requests
+from lxml import etree
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+def crawl():
+    base_url = "https://www.gourmetsociety.co.uk/ajax/markers.php?"
+    url = base_url + "restaurant="
+#   url += "&location=Edinburgh"
+    url += "&lat=52.50"
+    url += "&lng=-0.2"
+    url += "&radius=1000"
+    url += "&friday=false"
+    url += "&saturday=false"
+    url += "&sunday=false"
+    url += "&december=false"
+    url += "&offer="
+    url += "&cuisine="
+    
+    headers = {
+      'Referer' : 'https://www.gourmetsociety.co.uk/search.php?country=uk'
+    }
+    xml = requests.get(url, headers=headers).content
+    dom = etree.fromstring(xml)
+    
+    for marker in dom.xpath('/markers/marker'):
+        restaurant = {
+          'address' : base64.b64decode(marker.xpath('@address')[0]).decode('latin1'),
+          'address2' : base64.b64decode(marker.xpath('@address2')[0]),
+          'chain_logo' : base64.b64decode(marker.xpath('@chain_logo')[0]),
+          'city' : base64.b64decode(marker.xpath('@city')[0]),
+          'county' : base64.b64decode(marker.xpath('@county')[0]),
+          'cuisine' : base64.b64decode(marker.xpath('@cuisine')[0]),
+          'id' : base64.b64decode(marker.xpath('@id')[0]),
+          'latitude' : base64.b64decode(marker.xpath('@lat')[0]),
+          'longitude' : base64.b64decode(marker.xpath('@lng')[0]),
+          'name' : base64.b64decode(marker.xpath('@name')[0]).decode('latin1'),
+          'name4url' : base64.b64decode(marker.xpath('@name4url')[0]),
+          'postcode' : base64.b64decode(marker.xpath('@postcode')[0]),
+          'tel' : base64.b64decode(marker.xpath('@tel')[0]).decode('latin1'),
+          'thumb0' : base64.b64decode(marker.xpath('@thumb0')[0]),
+          'thumb1' : base64.b64decode(marker.xpath('@thumb1')[0]),
+          'type' : base64.b64decode(marker.xpath('@type')[0]),
+          'web' : base64.b64decode(marker.xpath('@web')[0])
+        }
+        # Saving data:
+        unique_keys = [ 'id' ]
+        scraperwiki.sqlite.save(unique_keys=['id'], data=restaurant)
+        # scraperwiki.sql.save(unique_keys, restaurant)
 
-# You don't have to do things with the ScraperWiki and lxml libraries. You can use whatever libraries are installed
-# on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+crawl()
